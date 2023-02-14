4971acb1f762efcda33d95f191a008b6a32f2a25
Ian <kotonefemc@gmail.com>
2023-02-14 15:34:24 +0000

    node class and driver done

--- Linked-Lists/main.cpp
+++ Linked-Lists/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "Node.h"
 
+template <class T>
+void print_list(Node<T>*);
+
 int main()
 {
+    Node<int>* root = new Node<int>(5);
+    root->setNext(new Node<int>(8));
+    root->getNext()->setNext(new Node<int>(10));
 
+    // Also could be done as the following:
+    // Node<int>* root = new Node<int>(5, new Node<int>(8, new Node<int>(10)))
+
+    print_list(root);
 
     return 0;
+}
+
+template<class T>
+void print_list(Node<T>* r) 
+{
+    while(r != nullptr) 
+    {
+        std::cout << r->getValue() << std::endl;
+        r = r->getNext();
+    }
 }
